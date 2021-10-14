cf61910d62b658f62578f4b1418253556a8257a7
Shekhar Gulati <shekhargulati84@gmail.com>
2021-10-15 01:01:43 +0530

    first version of chrome extension

---
+++ README.md
@@ -0,0 +1,19 @@
+# repo-age-chrome-extension
+
+This is a simple extension that adds shows a public Github repository first commit date and age. For example, below you can see Kubernetes first commit date and repo age.
+
+![](images/k8s.png)
+
+To use, clone this repository to your computer.
+
+```
+$ git clone git@github.com:shekhargulati/git@github.com:shekhargulati/repo-age-chrome-extension.git
+```
+
+Install with these steps:
+
+1. Go to the Chrome [extensions](chrome://extensions/) page.
+2. Turn on *Developer mode* to enable loading *unpacked extensions*.
+3. Add the extension by clicking *Load Unpacked* button, and select the source directory.
+
+After the extension is added, when you go to any public Github repository you will see the first commit date and repository age.
---
+++ content-script.js
@@ -0,0 +1,48 @@
+
+const loc = window.location.toString();
+const url = new URL(loc);
+const path = url.pathname;
+const parts = path.split("/").filter(p => p.length > 0);
+if (parts.length !== 2) {
+    console.log("Not running extension as it is not the repo home page");
+} else {
+    fetch(`https://api.github.com/repos/${parts[0]}/${parts[1]}/stats/contributors`)
+        .then(r => {
+            r.text().then(json => {
+                const parsed = JSON.parse(json);
+                const ts = parsed[0].weeks[0].w;
+                const firstCommitDate = new Date(ts * 1000);
+                const diffTime = Math.abs(new Date() - firstCommitDate);
+                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                const firstCommitDateEl = createElement("First Commit: " + firstCommitDate.toLocaleDateString('en-US', options));
+                const msg = `Repo Age: ${formatDays(diffDays)} days old`;
+                const repoAgeEl = createElement(msg);
+                const root = document.querySelector('div.BorderGrid-cell');
+                root.appendChild(firstCommitDateEl);
+                root.appendChild(repoAgeEl);
+            })
+        });
+
+}
+
+
+function formatDays(numberOfDays) {
+    const years = Math.floor(numberOfDays / 365);
+    const months = Math.floor(numberOfDays % 365 / 30);
+    const days = Math.floor(numberOfDays % 365 % 30);
+
+    const yearsDisplay = years > 0 ? years + (years == 1 ? " year, " : " years, ") : "";
+    const monthsDisplay = months > 0 ? months + (months == 1 ? " month, " : " months, ") : "";
+    const daysDisplay = days > 0 ? days + (days == 1 ? " day" : " days") : "";
+    return yearsDisplay + monthsDisplay + daysDisplay; 
+}
+
+function createElement(msg) {
+    var el = document.createElement("p");
+    el.classList.add('f4');
+    el.classList.add('mt-3');
+    el.innerText = msg;
+    return el;
+}
+
